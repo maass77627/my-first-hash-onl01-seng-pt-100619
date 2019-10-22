3bad33cd57c923010a1f91d57e0bb04afcf39327
kelly r maass <kmaass77627@gmail.com>
2019-10-22 02:55:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,7 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
+  your_hash = {"dog" => "spot", "cat" => "fluffy"}
+  end
+  your_hash
 
 
 def shipping_manifest
